44d50f90886d342e0d96ac3f180f00db4e58c549
Taha Bin Shafiq <mt4899192@gmail.com>
2024-12-26 23:40:36 +0500

    Add this Percentage Formula And round

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var userName = prompt("Enter your Name")
-alert("Welcome " + userName)
+alert("Welcome" + " " + userName)
 
 var math = +prompt("Enter your Maths num")
 var bio = +prompt("Enter your Biology num")
-var phy = +prompt("Enter your Physics num")
 
-var result = math + bio +phy
+var result = math + bio;
 
-alert("Your total number is " + result)
+alert("Your total number is" + " " + result)
+
+var percentage = result / 550 * 100;
+var round = Math.round(percentage);
+
+alert("Your Percentage is" + " " + round + "%")
 
